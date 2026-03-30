3ee93691b57ded535106d7ed04a1ae204b88d607
genecarles <genecarles@gmail.com>
2026-03-30 15:48:24 +0200

    Update enllacos.js

--- enllacos.js
+++ enllacos.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const enllacosHTML = `
         
         <div class="bloc-enllacos">
-            <p><a href="https://altervector.github.io/oleyajidinamics/PROMO.pdf"><img src="https://altervector.github.io/oleyajidinamics/Icon/Icopdf.png" alt="Pdf" class="icona-app">Descarrega el catàleg complet (PDF)</a></p>
+            <p><a href="https://altervector.github.io/oleyajidinamics/"><img src="https://altervector.github.io/oleyajidinamics/Icon/Icopdf.png" alt="Pdf" class="icona-app">Descarrega la Carta al complet (PDF)</a></p>
             <p><a href="https://www.instagram.com/oleyajivilafranca" target="_blank"><img src="https://altervector.github.io/oleyajidinamics/Icon/Icoinsta.png" alt="Instagram" class="icona-app">Instagram: @oleyajivilafranca</a></p>
             <p><a href="https://wa.me/"><img src="https://altervector.github.io/oleyajidinamics/Icon/icowhats.png" alt="Whatsapp" class="icona-app">Contacta per WhatsApp</a></p>
             <p><a href="mailto:"> <img src="https://altervector.github.io/oleyajidinamics/Icon/Icomail.png" alt="e-mail" class="icona-app">Envia un correu</a></p>
