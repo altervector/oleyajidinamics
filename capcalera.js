bff38020830864c64683139ba4df6464bd081fbe
genecarles <genecarles@gmail.com>
2026-03-31 20:29:21 +0200

    w

--- capcalera.js
+++ capcalera.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const headerHTML = `
         <div class="capcalera-principal">
             <div class="contenidor-logo">
-                <img src="https://altervector.github.io/oleyajidinamics/Icon/logotrans.jpg" alt="Logo Olé y Ají" class="logo">
+                <img src="https://altervector.github.io/oleyajidinamics/Icon/logotrans.png" alt="Logo Olé y Ají" class="logo">
             </div>
             <div class="text-capcalera">
                 <p>
