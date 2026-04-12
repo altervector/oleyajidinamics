53af5cffc8de9c7c7eebe1e97a7b62e72d9b8b93
genecarles <genecarles@gmail.com>
2026-04-12 21:27:13 +0200

    Update capcalera.js

--- capcalera.js
+++ capcalera.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
             // Blau permanent si som admin
             if (sessionStorage.getItem('adminMode') === 'true') {
                 logo.style.filter = "brightness(0) saturate(100%) invert(15%) sepia(98%) saturate(3019%) hue-rotate(202deg)"; 
-        document.body.style.setProperty('background-color', '#fb9d46', 'important');// Fons taronja suau
+        document.body.style.setProperty('background-color', '#e0ae80', 'important');// Fons taronja suau
                 
             }
 
